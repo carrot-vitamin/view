6e7ec90d3e9484911b08a8b61fc07c40231b5fa1
yin <602377321@qq.com>
2020-08-09 15:15:24 +0800

    u

--- src/main/java/com/github/carrotvitamin/view/ViewApplication.java
+++ src/main/java/com/github/carrotvitamin/view/ViewApplication.java
@@ -2,12 +2,18 @@ package com.github.carrotvitamin.view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ViewApplication {
+public class ViewApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ViewApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ViewApplication.class);
+    }
 }
